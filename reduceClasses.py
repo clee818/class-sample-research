0340e443259176ce4cf26beda26329afbe6c4fed
Cara <caralee@MacBook-Pro-99.lan>
2022-12-15 17:31:19 -0800

    Changed softmax to sigmoid

--- reduceClasses.py
+++ reduceClasses.py
@@ -1,18 +1,24 @@
 from torch.utils.data import Dataset, DataLoader
+import numpy as np
 
-class reduce(Dataset):
+class Reduce(Dataset):
     # reduces number of classes
     # takes in original dataset, target # of classes
-    def __init__(self, original_dataset, num_classes):        
+    def __init__(self, original_dataset, num_classes, nums=(0,1)):        
         
-        indices = original_dataset.train_labels < num_classes
+        indices = np.isin(original_dataset.train_labels, nums) 
+            
         self.images = original_dataset.train_data[indices==1].unsqueeze(1)
         self.labels = original_dataset.train_labels[indices==1]
+        self.nums = nums
         
-                 
     def __len__(self):
         return len(self.labels)
                  
     def __getitem__(self, index): 
         image = self.images[index].float()
-        return (image, self.labels[index])
+        if self.labels[index]==self.nums[0]:
+            label = 0
+        else:
+            label = 1
+        return (image, label)
